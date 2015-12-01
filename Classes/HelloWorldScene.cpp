8a01f979b6384b21fe74fde0ac16cd837b5e8854
Oinotna9 <ntonio9@hotmail.com>
2015-12-01 16:33:23 +0100

    sdjbgisdgasfjoiasoij
    TEMATO

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -1,7 +1,6 @@
 #include "HelloWorldScene.h"
 #include "puffer.h"
 
-#define COCOS2D_DEBUG 1
 USING_NS_CC;
 
 Scene* HelloWorld::createScene()
@@ -12,6 +11,7 @@ Scene* HelloWorld::createScene()
     // 'layer' is an autorelease object
     auto layer = HelloWorld::create();
 
+	
     // add layer as a child to scene
     scene->addChild(layer);	
 
@@ -30,9 +30,9 @@ bool HelloWorld::init()
         return false;
     }
 
-	
-	contador=0;
-
+	bool idle = true;
+	bool patada = false;
+    
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
 	 
@@ -44,134 +44,41 @@ bool HelloWorld::init()
     // add the sprite as a child to this layer
     this->addChild(sprite, 0);
 	
-	auto patadavoladora = puffer::createAnimation(5);
-	auto patadafinal = puffer::createAnimation(4);
-	auto puñetazo = puffer::createAnimation(3);
-	auto agachado = puffer::createAnimation(2);
-	auto patada = puffer::createAnimation(1);
-	auto idle = puffer::createAnimation(0);
+	//auto puf1 = puffer::createAnimation(1);
+	auto puf2 = puffer::createAnimation(0);
+
+
+	// run it
 	
-	this->addChild(idle);
-	this->addChild(patada);
-	this->addChild(agachado);
-	this->addChild(puñetazo);
-	this->addChild(patadafinal);
-	this->addChild(patadavoladora);
-
-	patadavoladora->setVisible(false);
-	patada->setVisible(false);
-	agachado->setVisible(false);
-	puñetazo->setVisible(false);
-	patadafinal->setVisible(false);
-
-	 auto eventListener = EventListenerKeyboard::create();
+	this->addChild(puf2);
 	
-	 this->schedule(schedule_selector(HelloWorld::update),0.9);
 
-     eventListener->onKeyPressed =[=](EventKeyboard::KeyCode keyCode, Event* event){
+	auto eventListener = EventListenerKeyboard::create();
+
+	
+     eventListener->onKeyPressed = [=](EventKeyboard::KeyCode keyCode, Event* event){
         
-		 switch(keyCode){
-
-          case EventKeyboard::KeyCode::KEY_A:
-
-				if (contador > 10){
-					patada->setVisible(true);
-					idle->setVisible(false);
-				}
-				else{
-
-					agachado->setVisible(true);
-					idle->setVisible(false);
-				}
-							 
-				break;        
-		  
-		  case EventKeyboard::KeyCode::KEY_S:
-
-				if (contador > 10){
-					puñetazo->setVisible(true);
-					idle->setVisible(false);
-				}
-				break;
-
-		  case EventKeyboard::KeyCode::KEY_D:
-
-				if (contador > 10){
-					patadafinal->setVisible(true);
-					idle->setVisible(false);
-				}
-				break;
+        switch(keyCode){
+           
+		case EventKeyboard::KeyCode::KEY_A:
+			puf2->removeFromParentAndCleanup(TRUE);
+			auto puf1 = puffer::createAnimation(1);
+			addChild(puf1);
 			
-		  case EventKeyboard::KeyCode::KEY_F:
 
-				if (contador > 10){
-					patadavoladora->setVisible(true);
-					idle->setVisible(false);
-				}
-				break;
-		 }
-		
-	 };	
-		 
-	
-	this->_eventDispatcher->addEventListenerWithSceneGraphPriority(eventListener,sprite);
-	
-	auto solucion = EventListenerKeyboard::create();
-	
-	    solucion->onKeyReleased =[=](EventKeyboard::KeyCode keyCode, Event* event){
-        
-                  
-          if (keyCode ==  EventKeyboard::KeyCode::KEY_A){
-				
-				
-				patada->setVisible(false);
-				idle->setVisible(true);
-				agachado->setVisible(false);
-				        
-						
-		 }
+		}
+	 };
 
-		  if (keyCode ==  EventKeyboard::KeyCode::KEY_S){			
-				
-				
-				idle->setVisible(true);
-				puñetazo->setVisible(false);
-				        
-						
-		 }
-			
-		  if (keyCode ==  EventKeyboard::KeyCode::KEY_D){			
-				
-				
-				idle->setVisible(true);
-				patadafinal->setVisible(false);
-				        
-						
-		 }
-		  if (keyCode ==  EventKeyboard::KeyCode::KEY_F){			
-				
-				
-				idle->setVisible(true);
-				patadavoladora->setVisible(false);
-				        
-						
-		 }
-	 };	
-	
-	 
-	
-	this->_eventDispatcher->addEventListenerWithSceneGraphPriority(solucion,sprite);
+	 this->_eventDispatcher->addEventListenerWithSceneGraphPriority(eventListener, sprite);
 	
 
 
-	return true;		
-	
-	
+
+
+return true;
+
 }
 
-void HelloWorld::update(float dt){		
-	
-	contador++;
-	
-	CCLOG("%d",contador);
-}
+
+
+
--- Classes/puffer.cpp
+++ Classes/puffer.cpp
@@ -16,12 +16,21 @@ Sprite *puffer::createAnimation(int numeron)
 
 	auto someSprite = Sprite::createWithSpriteFrameName("puffer1.png");
 
-	
+	Vector<SpriteFrame*> frames;
+	for (int i = 1; i <= 4; i++)
+	{
+		std::stringstream ss;
+		ss << "puffer" << i << ".png";
+		frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
+	}
+	// play the animation
+	Animation* anim1 = Animation::createWithSpriteFrames(frames, 0.2f);
+	//asomeSprite->runAction(Animate::create(anim1));
 
 	if (numeron==1){
 			// load all the animation frames into an array
 			Vector<SpriteFrame*> frames;
-			for (int i = 6; i <= 7; i++)
+			for (int i = 5; i <= 8; i++)
 			{
 				std::stringstream ss;
 				ss << "puffer" << i << ".png";
@@ -29,101 +38,49 @@ Sprite *puffer::createAnimation(int numeron)
 			}
 						// play the animation
 				Animation* anim = Animation::createWithSpriteFrames(frames, 0.2f);
-				someSprite->runAction(Animate::create(anim));
-				someSprite->setPosition(200, 100);	
-				someSprite->runAction(RepeatForever::create(Animate::create(anim)));
-	
+				someSprite->setPosition(100, 100);
+
+				// create a few actions.
+			
+				auto canim = Animate::create(anim);
+
+				
+
+				auto repite = Repeat::create(Animate::create(anim1),99999);
+
+				// create a sequence with the actions and callbacks
+				auto seq = Sequence::create(canim, repite, nullptr);
+
+				// run it
+				someSprite->runAction(seq);
+
+
+
+
+
 	}
 
-	if (numeron==2){
-			// load all the animation frames into an array
-			Vector<SpriteFrame*> frames;
-			for (int i = 21; i <= 28; i++)
-			{
-				std::stringstream ss;
-				ss << "puffer" << i << ".png";
-				frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
-			}
-						// play the animation
-				Animation* anim = Animation::createWithSpriteFrames(frames, 0.2f);
-				someSprite->runAction(Animate::create(anim));
-				someSprite->setPosition(200, 100);	
-				someSprite->runAction(RepeatForever::create(Animate::create(anim)));
-	
-	}
 
-	if (numeron ==0){
 
-			Vector<SpriteFrame*> frames;
-			for (int i = 1; i <= 4; i++)
-			{
-				std::stringstream ss;
-				ss << "puffer" << i << ".png";
-				frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
-			 }
-			// play the animation
-			Animation* anim = Animation::createWithSpriteFrames(frames, 0.2f);
-			someSprite->runAction(Animate::create(anim));
-			someSprite->setPosition(200, 100);	
-			someSprite->runAction(RepeatForever::create(Animate::create(anim)));
+
+
+	else{
+
+		
+			someSprite->setPosition(100, 100);	
+			someSprite->runAction(RepeatForever::create(Animate::create(anim1)));
+			
+
 	}
 	
-	if (numeron==3){
 
-			// load all the animation frames into an array
-			Vector<SpriteFrame*> frames;
-			for (int i = 18; i <= 20; i++)
-			{
-				std::stringstream ss;
-				ss << "puffer" << i << ".png";
-				frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
-			}
-						// play the animation
-				Animation* anim = Animation::createWithSpriteFrames(frames, 0.2f);
-				someSprite->runAction(Animate::create(anim));
-				someSprite->setPosition(200, 100);	
-				someSprite->runAction(RepeatForever::create(Animate::create(anim)));
 	
-	}
-
-	if (numeron==4){
-
-			// load all the animation frames into an array
-			Vector<SpriteFrame*> frames;
-			for (int i = 10; i <= 12; i++)
-			{
-				std::stringstream ss;
-				ss << "puffer" << i << ".png";
-				frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
-			}
-						// play the animation
-				Animation* anim = Animation::createWithSpriteFrames(frames, 0.2f);
-				someSprite->runAction(Animate::create(anim));
-				someSprite->setPosition(200, 100);	
-				someSprite->runAction(RepeatForever::create(Animate::create(anim)));
-	
-	}
-	if (numeron==5){
-
-			// load all the animation frames into an array
-			Vector<SpriteFrame*> frames;
-			for (int i = 14; i <= 15; i++)
-			{
-				std::stringstream ss;
-				ss << "puffer" << i << ".png";
-				frames.pushBack(cacher->getSpriteFrameByName(ss.str()));
-			}
-						// play the animation
-				Animation* anim = Animation::createWithSpriteFrames(frames, 0.2f);
-				someSprite->runAction(Animate::create(anim));
-				someSprite->setPosition(200, 100);	
-				someSprite->runAction(RepeatForever::create(Animate::create(anim)));
-	
-	}
 	
 	return someSprite;
 
 	}
+
+
 
 
 
